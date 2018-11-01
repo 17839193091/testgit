8304cbe6b5a9e0645540f702940742fd631cb2ea
hu.dongfei <hu.dongfei@ustcinfo.com>
2018-11-01 15:59:25 +0800

    第一次提交

--- src/main/java/cn/hdf/test/Test.java
+++ src/main/java/cn/hdf/test/Test.java
@@ -2,12 +2,12 @@ package cn.hdf.test;
 
 /**
  * 描述:
- *
+ *  测试git提交，尝试学习解决冲突
  * @author hudongfei
  * @create 2018-11-01 15:43
  */
 public class Test {
     public static void main(String[] args) {
-        
+
     }
 }
